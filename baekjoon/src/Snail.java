7c2f3e9304648f09a92bcfe311dcdf04840d54d3
jooheexxxx <silverlining.jh@gmail.com>
2023-02-03 13:49:48 +0900

    step7-4 / 2869번

---
+++ baekjoon/src/Snail.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Snail {
+
+    public static void main(String args[]) throws IOException {
+                
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 문자열 자르기 : 구분자 이용
+        String[] s = br.readLine().split(" ");        
+        double a = Integer.parseInt(s[0]);
+        double b = Integer.parseInt(s[1]);
+        double v = Integer.parseInt(s[2]);
+
+        // 계산식 (낮에 정상에 도달하면 밤에 미끄러지지 않음)
+        // v >= a*day - b*(day-1) 
+        double day = (v-b)/(a-b); // 데이터형을 float로 할 경우 예제3에서 오답 도출
+
+        if(day == (int) day){ // 답이 정수인 경우
+            System.out.println((int)day);
+        }else{
+            System.out.println((int)day+1);
+        }
+    }    
+}
