bcfb5305d728f95f802200839df0087854d50de6
jooheexxxx <silverlining.jh@gmail.com>
2023-02-15 11:30:58 +0900

    step8-6 / 9020번 / 시간 초과 수정

--- baekjoon/src/GoldbachAnswer.java
+++ baekjoon/src/GoldbachAnswer.java
@@ -2,28 +2,18 @@
 
 import java.io.*;
 
-public class Goldbach {
+public class GoldbachAnswer {
     public static void main (String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-        // 테스트 케이스
+        // 테스트 케이스 입력 받기
         int temp[] = new int [t];
         for(int i=0;i<t;i++){
             int num = Integer.parseInt(br.readLine());
             temp[i] = num;
-        }
-        
+        }        
         for(int n=0;n<t;n++){
-            int num = temp[n];
-            // 이중 배열 선언, arr[x][n-x] = x와 n-x의 차 >> 불필요 항목으로 해당 과정 제거하여 시간 단축 가능
-            int arr[][] = new int [num+1][num+1];
-            for(int i=0;i<=num;i++){
-                if(i<=num/2){
-                    arr[i][num-i] = num-i*2;
-                }else{
-                    arr[i][num-i] = i*2-num;
-                }                
-            }            
+            int num = temp[n];           
             // 소수 판별 : 에라토스테네스의 체
             boolean[] prime = new boolean [num+1];
             prime[0] = true;
@@ -35,12 +25,11 @@ public class Goldbach {
             }
             // 골드바흐 파티션 출력
             int min = 10000;
-            for(int i=0;i<=num/2;i++){ // arr 배열은 대칭을 이루므로 num/2 까지만 조회하면 됨
+            for(int i=0;i<=num/2;i++){ // num/2 이후로 같은 숫자가 반복되므로 num/2 까지만 조회
                 if(prime[i]==false && prime[num-i]==false){
-                    //System.out.println(num+" "+arr[i][num-i]);
                     // 두 소수의 차이가 가장 작은 경우 찾기
-                    if(min>arr[i][num-i]){
-                        min = arr[i][num-i];
+                    if(min>num-i*2){
+                        min = num-i*2;
                     }                    
                 }
             }
