7e58a1135ffffba6c6655496f20ea45eae9fe5f6
jooheexxxx <silverlining.jh@gmail.com>
2023-02-17 15:21:37 +0900

    step10-3 / 25305번

---
+++ baekjoon/src/Cutline.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Cutline {
+    public static void main (String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 첫째 줄
+        StringTokenizer first = new StringTokenizer(br.readLine());
+        int num = Integer.parseInt(first.nextToken());
+        int prize = Integer.parseInt(first.nextToken());
+        // 둘째 줄
+        StringTokenizer second = new StringTokenizer(br.readLine());
+        int arr[] = new int [num];
+        for(int i=0;i<num;i++){
+            int score = Integer.parseInt(second.nextToken());
+            arr[i] = score;
+        }
+        // 내림차순 변경
+        int t;        
+        for(int i=0;i<num;i++){
+            for(int j=0;j<i;j++){
+                if(arr[i]>arr[j]){
+                    t = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = t;
+                }
+            }
+        }
+        // 커트라인 출력
+        System.out.println(arr[prize-1]);
+    }
+}
